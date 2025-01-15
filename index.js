13c47141ac76711a5ecc37fccd611e09962c3369
nemo <jx007s@gmail.com>
2025-01-15 15:03:49 +0900

    01_15_exam

--- index.js
+++ index.js
@@ -4,6 +4,8 @@
 
 import {AppRegistry} from 'react-native';
 import App from './App';
+import UI_1 from './components/ui_1';
+import UI_Exam from './components/ui_exam';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => UI_Exam);
