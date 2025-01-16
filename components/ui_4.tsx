ae90eb066a6af8995e1820245f85a3bccd8c2af1
nemo <jx007s@gmail.com>
2025-01-16 10:21:02 +0900

    01_16_bar

--- components/ui_4.tsx
+++ components/ui_4.tsx
@@ -1,7 +1,29 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import {SafeAreaView, Switch, View } from 'react-native';
+import {Bar} from 'react-native-progress'
 
 function ui_4(props) {
+
+    const [myPro, setMyPro] = useState(0)
+
+
+    useEffect(()=>{
+        console.log("최초 실행시 작업하지");
+
+
+        const timer = setInterval(()=>{
+            setMyPro((ppp) => {
+                if(ppp>=1){
+                    clearInterval(timer)
+                    return 1
+                }
+                return ppp + 0.05
+            })
+
+        },1000)
+
+    },[])
+
 
     const [isToggle, setToggle] = useState(false);
 
@@ -10,11 +32,12 @@ function ui_4(props) {
         // previousState  : 기존의 값
         setToggle((previousState)=>!previousState)
     }
-
+    //   0 <= progress <= 1
     return (
+
         <SafeAreaView>
             <Switch value={isToggle} onValueChange={toggleSwitch}/>
-            
+            <Bar width={300} progress={myPro}/>  
         </SafeAreaView>
     );
 }
